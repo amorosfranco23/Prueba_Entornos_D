8a8f34365b74116dcd57fea931b402cb2c82b675
Julia <amorosfranco23@iespabloserrano.com>
2025-11-20 10:06:06 +0100

    Primera version del programa

---
+++ src/proyecto_prueba/primer_programa.java
@@ -0,0 +1,19 @@
+package proyecto_prueba;
+
+public class primer_programa {
+
+	public static void main(String[] args) {
+		int variable = 5;
+		variable++;
+		variable = variable + 10;
+		System.out.println("Variable vale: " + variable);
+		
+		int contador = 0;
+		while (contador <= 10) {
+			System.out.println("El numero es: " + contador);
+			contador++;
+		}
+
+	}
+
+}
